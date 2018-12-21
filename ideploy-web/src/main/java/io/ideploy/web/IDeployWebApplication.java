bb9d6d2d56a3cd383f0c0a1dbf00e766abbc6d25
lidongyao <lidongyao@tigerbrokers.com>
2018-12-21 16:31:59 +0800

    移除代码

--- ideploy-web/src/main/java/io/ideploy/web/IDeployWebApplication.java
+++ ideploy-web/src/main/java/io/ideploy/web/IDeployWebApplication.java
@@ -1,18 +1,20 @@
 package io.ideploy.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author: code4china
- * @description: 服务启动入口
- * @date: Created in 14:46 2018/12/21
+ * @author code4china
+ * @description 服务启动入口
+ * @date Created in 14:46 2018/12/21
  */
+@EnableAutoConfiguration
 @SpringBootApplication
 public class IDeployWebApplication {
 
     public static void main(String[]args){
-        SpringApplication.run(IDeployWebApplication.class);
+        SpringApplication.run(IDeployWebApplication.class, args);
     }
 
 }
